6c7c55c6b0ac32eb65d8fbeb7a3e73cb49fb7e77
Maurice Fonk <git@naneau.nl>
2010-10-17 17:07:41 +0200

    added css based highlighting (basic version, geshi sucks)

--- Filter/Code.php
+++ Filter/Code.php
@@ -23,6 +23,13 @@ use Application\MadoquaBundle\Filter\Filter;
 class Code implements Filter
 {
     /**
+     * regex to match <pre><code>#lang blocks
+     *
+     * @var string
+     */
+    private static $regex = '/\<pre\>\<code\>(\#[A-Za-z0-9_]+\n)(.*)\<\/code\>\<\/pre\>/sU';
+    
+    /**
      * filter
      *
      * @param string $text 
@@ -30,23 +37,27 @@ class Code implements Filter
      */
     public function filter($text)
     {
-        $text = preg_replace_callback(
-            '/\<code\>(\#[A-Za-z0-9_]+\n)(.*)\<\/code\>/sU', 
-            function($matches){
+        return preg_replace_callback(
+            self::$regex,
+            
+            function($matches) {
                 if (isset($matches[1]) && isset($matches[2])) {
                     //we have a #lang attribute in our <code />
-                    $language = trim($matches[1], "#\n");
-                    $code = trim($matches[2], "\n");
-                    $geshi = new \GeSHi($code, $language);
                     
+                    $language = trim($matches[1], "#\n");
+                    $code = html_entity_decode(trim($matches[2], "\n"));
+                    
+                    $geshi = new \GeSHi($code, $language);
+                    $geshi->set_header_type(GESHI_HEADER_PRE_VALID);
+                    $geshi->set_overall_class('highlighted');
+                    $geshi->enable_classes();
                     return $geshi->parse_code();
+                    
                 }
                 
-                return $matches[0];
+                return $matches[0]; //for some reason this went wrong
             },
             $text
         );
-        
-        return $text;
     }
 }
