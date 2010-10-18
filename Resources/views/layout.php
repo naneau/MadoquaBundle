11ee88142ce7cebaaad0b787cd5dfcc0fb8a506e
Maurice Fonk <git@naneau.nl>
2010-10-18 14:03:44 +0200

    IE HTML5 fix (through js? :S)

--- Resources/views/layout.php
+++ Resources/views/layout.php
@@ -14,7 +14,11 @@
     <meta charset="UTF-8">
     
     <link rel="stylesheet" type="text/css" href="<?php echo $view['assets']->getUrl('/bundles/madoqua/css/naneau.css'); ?>" media="all">  
-    
+    <!--[if lte IE 8]>
+    <script type="text/javascript">
+    (function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(',');for(var i=0;i<e.length;i++){document.createElement(e[i])}})()
+    </script>
+    <![endif]-->    
     <title>Naneau</title>
 </head>
 
