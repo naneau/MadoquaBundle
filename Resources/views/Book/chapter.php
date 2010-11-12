bcf375b021736ec2bdf294f327bfadc0aa052b4c
Maurice Fonk <git@naneau.nl>
2010-11-12 17:01:21 +0100

    styled the things a bit

--- Resources/views/Book/chapter.php
+++ Resources/views/Book/chapter.php
@@ -17,12 +17,13 @@ $view->extend('MadoquaBundle::layout');
     </h1>
     
     <?php if ($chapter->hasParent()) : ?>
-    <a href="<?php echo $view['router']->generate('book_chapter', array('path' => $chapter->getParent()->getPath())); ?>">
-        <?php echo $chapter->getParent()->getName(); ?>
+    <a class="chapter-up" href="<?php echo $view['router']->generate('book_chapter', array('path' => $chapter->getParent()->getPath())); ?>">
+        Terug naar <?php echo $chapter->getParent()->getName(); ?>
     </a>
     <?php endif; ?>
     
     <?php if (count($chapter->getChapters()) > 0) : ?>
+    <h2>Hoofdstukken</h2>
     <ul class="chapters">
 
         <?php foreach($chapter->getChapters() as $subChapter) : ?>
@@ -39,7 +40,7 @@ $view->extend('MadoquaBundle::layout');
     <?php if (count($chapter->getPages()) > 0) : ?>
 
     <ul class="pages">
-
+        <h2>Pagina's</h2>
         <?php foreach($chapter->getPages() as $page) : ?>
         <li>
             <a href="<?php echo $view['router']->generate('book_page', array('path' => $page->getPath())); ?>">
