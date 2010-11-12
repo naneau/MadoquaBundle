97027097fe2963b990229a79f606eea0b835a541
Maurice Fonk <git@naneau.nl>
2010-11-12 17:08:33 +0100

    added "back" link to page read

--- Resources/views/Book/page.php
+++ Resources/views/Book/page.php
@@ -9,5 +9,9 @@
 $view->extend('MadoquaBundle::layout'); 
 ?>
 <section class="page">
+    <a class="chapter-up" href="<?php echo $view['router']->generate('book_chapter', array('path' => $page->getChapter()->getPath())); ?>">
+        Terug naar <?php echo $page->getChapter()->getName(); ?>
+    </a>
+    
     <?php echo $page->getRawValue()->getParsedText(); ?>
 </section>
